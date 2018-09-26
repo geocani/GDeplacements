380a651ef74d29a3f039c5dde35bfa6aa97c0691
Canivet Geoffrey <35733059+geocani@users.noreply.github.com>
2018-09-26 14:49:56 +0200

    maj

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap core CSS -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!--external css-->
+  <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/style-responsive.css" rel="stylesheet">
+  <title>frais kilometrique</title>
+
+</head>
+
+<body>
+  <section id="container">
+    <!-- **********************************************************************************************************************************************************
+        HEADER MENU TOP
+        *********************************************************************************************************************************************************** -->
+    <!--HEADER MENU TOP FIX-->
+    <header class="header black-bg">
+      <div class="sidebar-toggle-box">
+        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+      </div>
+      <!--LOGO-->
+      <a href="index.html" class="logo"><b>DASH<span>IO</span></b></a>
+      <!--MENU TOP-->
+      <div class="top-menu">
+        <ul class="nav pull-right top-menu">
+          <li><a class="logout" href="login.html">Logout</a></li>
+        </ul>
+      </div>
+    </header>
+    <!-- **********************************************************************************************************************************************************
+        SIDEBAR MENU LEFT
+        *********************************************************************************************************************************************************** -->
+    <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+          <h5 class="centered">MENU LEFT</h5> <!-- TITRE-->
+          <!-- LIEN SIMPLE-->
+          <li class="mt"> 
+            <a href="index.html">
+              <i class="fa fa-dashboard"></i>
+              <span>LIEN SIMPLE</span>
+              </a>
+          </li>
+          <!-- LIEN ACCORDEON-->
+          <li class="sub-menu">
+            <a class="active" href="javascript:;">
+              <i class="fa fa-desktop"></i>
+              <span>UI Elements</span>
+              </a>
+            <ul class="sub">
+              <li><a href="#">General</a></li>
+            </ul>
+          </li>
+          <!-- LIEN ACCORDEON-->
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-cogs"></i>
+              <span>Components</span>
+              </a>
+            <ul class="sub">
+              <li><a href="#">Grids</a></li>
+              <li><a href="#">Calendar</a></li>
+            </ul>
+          </li>
+          <!-- LIEN ACCORDEON-->
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-book"></i>
+              <span>Extra Pages</span>
+              </a>
+            <ul class="sub">
+              <li><a href="#">Blank Page</a></li>
+              <li><a href="#">Login</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </aside>
+    <!--sidebar end-->
+    <!-- **********************************************************************************************************************************************************
+        MAIN
+        *********************************************************************************************************************************************************** -->
+    <!--main content start-->
+    <section id="main-content">
+      <section class="wrapper">
+        <div class="row mt">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex accusamus nihil aliquam debitis ut eveniet optio, 
+            deserunt dolore deleniti perferendis non ratione error laudantium quaerat. Nulla inventore fuga a eos!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda qui, nulla velit illo harum veritatis, quidem incidunt eligendi tempore atque 
+            aliquam accusantium quibusdam rem aut vitae consequatur provident? Nobis, numquam.</p>
+        </div>
+      </section>
+    </section>
+
+
+    <!-- **********************************************************************************************************************************************************
+        FOOTER
+        *********************************************************************************************************************************************************** -->
+    
+    <!--footer end-->
+  </section>
+  <!-- js placed at the end of the document so the pages load faster -->
+  <script src="lib/jquery/jquery.min.js"></script>
+
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script class="include" type="text/javascript" src="lib/jquery.dcjqaccordion.2.7.js"></script>
+  <script src="lib/jquery.scrollTo.min.js"></script>
+  <script src="lib/jquery.nicescroll.js" type="text/javascript"></script>
+  <!--common script for all pages-->
+  <script src="lib/common-scripts.js"></script>
+</body>
+
+</html>
