0e1e2bac2ac67b24b890e8664e65aa7cdc29afa1
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-10-14 18:23:39 +0100

    form

--- form.php
+++ form.php
@@ -19,9 +19,9 @@
 
 <body>
     <?php
-        session_start();
+        // session_start();
         // Connexion BDD
-        $bdd = new PDO("mysql: host=localhost; dbname=frais; charset=utf8", "root", "");
+        // $bdd = new PDO("mysql: host=localhost; dbname=frais; charset=utf8", "root", "");
 
         // $user_id = $_GET['id'];
 
@@ -36,11 +36,11 @@
                     <img src="img/logo.png" alt="Logo" width="150px">
                 </li>
             </ul>
-            <!-- <ul class="titre-center">
+            <ul class="titre-center">
                 <li>
-                    <h2>Frais kilométrique</h2>
+                    <h2>Frais de déplacement et de séjour</h2>
                 </li>
-            </ul> -->
+            </ul>
             <ul class="nav pull-right top-menu">
                 <li>
                     <div class="sidebar-toggle-box">
@@ -57,9 +57,9 @@
     <div id="sidebar" class="nav-collapse">
         <ul class="sidebar-menu" id="nav-accordion">
             <li ><h5 class="">PROFIL</h5></li> <!-- TITRE-->
-            <li><p class="text-color"><i class="fa fa-user ico-size"></i><?php echo $_SESSION['nom']." ".$_SESSION['prenom']; ?></p></li>
-            <li><p class="text-color"><i class="fa fa-briefcase ico-size"></i><?php echo $_SESSION['service'];?></p></li>
-            <li><p class="text-color"><i class="fa fa-drivers-license ico-size"></i><?php echo $_SESSION['grade'];?></p></li>
+            <li><p class="text-color"><i class="fa fa-user ico-size"></i><?php //echo $_SESSION['nom']." ".$_SESSION['prenom']; ?></p></li>
+            <li><p class="text-color"><i class="fa fa-briefcase ico-size"></i><?php //echo $_SESSION['service'];?></p></li>
+            <li><p class="text-color"><i class="fa fa-drivers-license ico-size"></i><?php //echo $_SESSION['grade'];?></p></li>
             <!-- OUTILS-->
             <li ><h5 class="">OUTILS</h5></li>
             <li class="sub-menu">
@@ -92,72 +92,79 @@
 
 <!-- FORM -->
 <div class="container form-frais">
+    <div class="titre-page"><h2>FORMULAIRE</h2></div>
     <form>
-        <!---->
-        <h3>Date et heure de départ:</h3>
+        <!--Date et heure de départ-->
+        <h4>Date et heure de départ.</h4>
         <div class="form-row">
             <div class="form-group col-md-4">
-                <label for="inputEmail4">Date</label>
-                <input type="time" class="form-control" id="timepicker" placeholder="Heure">
+                <label for="date-trajet">Date:</label>
+                <input type="date" class="form-control" id="date-trajet" placeholder="Heure">
             </div>
             <div class="form-group col-md-4">
-                <label for="inputPassword4">Départ</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="résidence administrative">
+                <label for="heure-administrative">Départ:</label>
+                <input type="text" class="form-control" id="heure-administrative" placeholder="résidence administrative">
             </div>
             <div class="form-group col-md-4">
-                <label for="inputPassword4">Départ</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Domicile">
+                <label for="heure-domicile">Départ:</label>
+                <input type="text" class="form-control" id="heure-domicile" placeholder="Domicile">
             </div>
         </div>
-        <!---->
-        <h3>Commune visitée:</h3>
+        <!--Commune visitée-->
+        <h4>Commune visitée.</h4>
         <div class="form-row">
             <div class="form-group col-md-3">
-                <label for="inputEmail4">Nom</label>
-                <input type="time" class="form-control" id="timepicker" placeholder="Heure">
+                <label for="nom-commune">Nom:</label>
+                <input type="time" class="form-control" id="nom-commune" placeholder="Ex: Namur">
             </div>
             <div class="form-group col-md-3">
-                <label for="inputPassword4">Heure d'arrivée</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="résidence administrative">
+                <label for="heure-arrivee">Heure d'arrivée:</label>
+                <input type="time" class="form-control" id="heure-arrivee" placeholder="">
             </div>
             <div class="form-group col-md-3">
-                <label for="inputPassword4">Heure de départ</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Domicile">
+                <label for="heure-depart">Heure de retour:</label>
+                <input type="time" class="form-control" id="heure-depart" placeholder="">
             </div>
             <div class="form-group col-md-3">
-                <label for="inputPassword4">Heure de fin de mission</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Domicile">
+                <label for="fin-mission">Heure de fin de mission:</label>
+                <input type="time" class="form-control" id="fin-mission" placeholder="">
             </div>
         </div>
-        <!---->
-        <h3>Kilomètre parcourus:</h3>
+        <!--KM parcourus-->
+        <h4>Kilomètres parcourus.</h4>
         <div class="form-row">
             <div class="form-group col-md-4">
-                <label for="inputEmail4">Dans l'agomération</label>
-                <input type="time" class="form-control" id="timepicker" placeholder="Heure">
+                <label for="km-aglomeration">Dans l'agomération:</label>
+                <input type="number" class="form-control" id="km-aglomeration" placeholder="Ex: 10">
             </div>
             <div class="form-group col-md-4">
-                <label for="inputPassword4">Hors aglomération</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="résidence administrative">
+                <label for="km-hors">Hors aglomération:</label>
+                <input type="number" class="form-control" id="km-hors" placeholder="Ex: 10">
             </div>
             <div class="form-group col-md-4">
-                <label for="inputPassword4">Transport en commun</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="Domicile">
+                <label for="transport">Transport en commun:</label>
+                <input type="number" class="form-control" id="transport" placeholder="Ex: 2.50">
             </div>
         </div>
-        <!---->
+        <!--Motif-->
         <div class="form-group">
-            <label for="inputAddress">Motif</label>
-            <textarea class="form-control" id="inputAddress" placeholder="1234 Main St">
+            <label for="motif">Motif:</label>
+            <textarea class="form-control" id="motif" rows="3"></textarea>
         </div>
         <!---->
         <div class="form-group">
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">Check me out</label>
+                <p>
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">Je déclare de ne pas bénéficier d'un libre parcours et/ou de bénéficier d'une rédution sur les chemins de fer et autobus.</label>
+                </p>
+                <p>
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">J'affirme sur l'honneur que cette déclaration est sincère et complète à la somme de quarante et un euro, dix-huit cents.</label>
+                </p>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-primary">Envoyer le formulaire</button>
     </form>
 </div>
 
