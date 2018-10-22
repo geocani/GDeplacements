16eaba758b99521450510185a946015938f59e5e
Canivet Geoffrey <35733059+geocani@users.noreply.github.com>
2018-10-22 15:27:22 +0200

    maj

--- view/visualisation.php
+++ view/visualisation.php
@@ -17,74 +17,71 @@
         $status_trajet = $trajet['status_trajet'];
     }
 ?>
-
 <section id="main-content">
     <section class="wrapper">
         <div class="center container">
             <div class="titre-page">
                 <h2><i class="fa fa-clock-o pad-ico-profil"></i><span>Recapitulatif</span><h2>
             </div>
-            <div class="recap">
-                <div class="col-md-1">
-                    <!-- VIDE -->
-                </div>
-                <div class="col-md-10">
-                    <table class="table table-sm table-striped shad">
-                        <tbody>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Date: </span></p></th>
-                                <td><p class="rec"><?php echo $date_trajet; ?></p> </td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Départ administratif: </span></p></th>
-                                <td><p class="rec"><?php echo $heure_administrative; ?></p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Départ domicile: </span></p></th>
-                                <td><p class="rec"><?php echo $heure_domicile; ?></p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Commune visitée: </span></p></th>
-                                <td><p class="rec"><?php echo $nom_commune; ?></p></td>
-                            </tr>
-                                <tr>
-                                <th scope="row"><p class="rec"><span class="str">heure d'arrivée: </span></p></th>
-                            <td><p class="rec"><?php echo $heure_arrivee; ?></p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">heure de départ: </span></p></th>
-                                <td><p class="rec"><?php echo $heure_depart; ?></p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Fin de mission: </span></p></th>
-                                <td><p class="rec"><?php echo $fin_mission; ?></p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Km dans aglomération: </span></p></th>
-                                <td><p class="rec"><?php echo $km_aglomeration; ?> Km</p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Km hors aglomération: </span></p></th>
-                                <td><p class="rec"><?php echo $km_hors; ?> km</p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Transport: </span></p></th>
-                                <td><p class="rec"><?php echo $transport; ?> €</p></td>
-                            </tr>
-                            <tr>
-                                <th scope="row"><p class="rec"><span class="str">Motif: </span></p></th>
-                                <td><p class="rec"><?php echo $motif; ?></p></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <a href="historique.php?id_trajet=<?php echo $_SESSION['id_agent']; ?>"><button type="button" class="btn btn-primary">Retour</button></a>
-                    <a href="modification_trajet.php?id_trajet=<?php echo $trajet['id_trajet']; ?>"><button type="button" class="btn btn-primary">Modifier</button></a>  
-                    <a href="historique.php?id_trajet=<?php echo $_SESSION['id_agent']; ?>"><button type="button" class="btn btn-primary">Imprimer</button></a> 
-                </div>
-                <div class="col-md-1">
-                    <!-- VIDE -->
-                </div>
-            </div>     
+            <table class="table table-sm table-striped shad">
+                <tbody>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">ID: </span></p></th>
+                        <td><p class="rec"><?php echo $id_trajet; ?></p> </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Status: </span></p></th>
+                        <td><p class="rec"><?php echo $status_trajet; ?></p> </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Date: </span></p></th>
+                        <td><p class="rec"><?php echo $date_trajet; ?></p> </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Départ administratif: </span></p></th>
+                        <td><p class="rec"><?php echo $heure_administrative; ?></p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Départ domicile: </span></p></th>
+                        <td><p class="rec"><?php echo $heure_domicile; ?></p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Commune visitée: </span></p></th>
+                        <td><p class="rec"><?php echo $nom_commune; ?></p></td>
+                    </tr>
+                        <tr>
+                        <th scope="row"><p class="rec"><span class="str">heure d'arrivée: </span></p></th>
+                    <td><p class="rec"><?php echo $heure_arrivee; ?></p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">heure de départ: </span></p></th>
+                        <td><p class="rec"><?php echo $heure_depart; ?></p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Fin de mission: </span></p></th>
+                        <td><p class="rec"><?php echo $fin_mission; ?></p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Km dans aglomération: </span></p></th>
+                        <td><p class="rec"><?php echo $km_aglomeration; ?> Km</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Km hors aglomération: </span></p></th>
+                        <td><p class="rec"><?php echo $km_hors; ?> km</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Transport: </span></p></th>
+                        <td><p class="rec"><?php echo $transport; ?> €</p></td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><p class="rec"><span class="str">Motif: </span></p></th>
+                        <td><p class="rec"><?php echo $motif; ?></p></td>
+                    </tr>
+                </tbody>
+            </table>
+            <a href="historique.php?id_trajet=<?php echo $_SESSION['id_agent']; ?>"><button type="button" class="btn btn-primary">Retour</button></a>
+            <a href="modification_trajet.php?id_trajet=<?php echo $id_trajet; ?>"><button type="button" class="btn btn-primary">Modifier</button></a>  
+            <a class="hidden-phone" href="historique.php?id_trajet=<?php echo $_SESSION['id_agent']; ?>"><button type="button" class="btn btn-primary">Imprimer</button></a>
         </div>
     </section>
 </section>
